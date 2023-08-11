17127c4caa84fdffa29156d6a68339d2969d3246
khalid Ammar <khalidammarofficial@gmail.com>
2023-08-10 19:35:01 -0700

    C programm that play special line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  C program that prints a special line
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}

f9be312c4a5e9e0b28c3db0be69ae7561d50e59e
khalid Ammar <khalidammarofficial@gmail.com>
2023-08-16 18:26:05 -0700

    Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 	int z, y, x, w;
 
-	for (z = 0; z <=2; z++)
+	for (z = 0; z <= 2; z++)
 	{
-		for (y = 0; y <=9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			if ((z <= 1 && y <= 9) || (z <= 2 && y <= 3))
 			{
 				for (x = 0; z <= 5; x++)
 				{
-					for (w = 0; w <=9; w++)
+					for (w = 0; w <= 9; w++)
 					{
 						_putchar(z + '0');
 						_putchar(y + '0');
